fc2e381d1e7d4d51e6311e60c086398a0ca1b210
Eric Oliveira <oiericoi@hotmail.com>
2023-10-21 18:46:55 -0300

    feat: add db connection

---
+++ internal/db/connection.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ericoliveiras/gate-guard/internal/config"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func Init(config *config.Config) *sqlx.DB {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		config.DB.Host, config.DB.User, config.DB.Password, config.DB.Name, config.DB.Port)
+
+	db, err := sqlx.Open(config.DB.Driver, dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database. %v", err)
+	}
+
+	return db
+}
