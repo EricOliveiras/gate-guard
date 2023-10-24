a52966a7b0fb5fdc1cf1a80ead435fd699c94c6c
Eric Oliveira <oiericoi@hotmail.com>
2023-10-24 14:08:44 -0300

    refactor: modify init db function

--- internal/db/connection.go
+++ internal/db/connection.go
@@ -9,7 +9,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func Init(config *config.Config) *sqlx.DB {
+func Init(config *config.Config) (*sqlx.DB, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		config.DB.Host, config.DB.User, config.DB.Password, config.DB.Name, config.DB.Port)
 
@@ -18,5 +18,5 @@ func Init(config *config.Config) *sqlx.DB {
 		log.Fatalf("Failed to connect to the database. %v", err)
 	}
 
-	return db
+	return db, nil
 }
