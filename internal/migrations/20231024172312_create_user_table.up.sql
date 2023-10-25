092b66418269c770d56663104cd664b2e1ac62a8
Eric Oliveira <oiericoi@hotmail.com>
2023-10-25 10:27:19 -0300

    feat: add user migrations

---
+++ internal/migrations/20231024172312_create_user_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+  id UUID PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100),
+  document_id VARCHAR(50) NOT NULL UNIQUE,
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
+);
