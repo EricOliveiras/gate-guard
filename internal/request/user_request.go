1ffb1d80e9a566fd0cd946de66f557e611cf1bfa
Eric Oliveira <oiericoi@hotmail.com>
2023-10-28 14:11:54 -0300

    refactor: modify create user request

--- internal/request/user_request.go
+++ internal/request/user_request.go
@@ -1,12 +1,7 @@
 package request
 
-import "time"
-
 type CreateUser struct {
-	ID         string    `json:"id"`
-	FirstName  string    `json:"first_name"`
-	LastName   string    `json:"last_name"`
-	DocumentId string    `json:"document_id"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	DocumentId string `json:"document_id"`
 }
